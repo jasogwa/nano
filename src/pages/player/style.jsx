855777d142237a0319f537f2cd22ff8d292c0892
Jonas <jonaschinagorom@gmail.com>
2022-10-05 11:27:46 +0200

    styling

--- src/pages/player/style.jsx
+++ src/pages/player/style.jsx
@@ -1,33 +1,45 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 60%;
+    height: 100vh;
+    width: 600px;
+    background: #ffffff;
     margin: auto;
 `;
 
 export const Title = styled.h1`
-    align-content: center;
+    margin-top: 5px;
     text-align: center;
+    color: #ffa600e8;
 `;
+
+export const Video = styled.div``;
 
 export const Wrapper = styled.div`
     display: flex;
-    justify-content: space-between;
+    flex-wrap: wrap;
 `;
 
-export const Wapper1 = styled.div`
-    flex: 1;
+export const WrapLeft = styled.div`
+    flex: 2;
+    justify-content: left;
+`;
+
+export const WrapRight = styled.div`
+    justify-content: right;
 `;
 
 export const Button = styled.button`
-    color: palevioletred;
+    color: #ffa600e8;
     font-size: 1em;
-    margin: 1em;
+    margin-right: 1em;
+    margin-top: 3px;
     padding: 0.25em 1em;
-    border: 2px solid palevioletred;
+    border: 2px solid #ffa600e8;
     border-radius: 3px;
     cursor: pointer;
     &:hover{
-      background-color: #fccbcb;
+      background-color: #ffa600e8;
+      color: white;
     }
 `;
